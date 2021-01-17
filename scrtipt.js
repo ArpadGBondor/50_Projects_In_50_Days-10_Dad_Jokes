7a7420606d7a2af8fb1fc13ca94d1945fc91cf3d
Arpad G. Bondor <arpad.g.bondor@gmail.com>
2021-01-17 13:46:56 +0000

    Add files via upload

--- scrtipt.js
+++ scrtipt.js
@@ -6,14 +6,18 @@ jokeBtn.addEventListener('click', () => generateJoke());
 generateJoke();
 
 async function generateJoke() {
-  const config = {
-    headers: {
-      Accept: 'application/json',
-    },
-  };
-  const res = await fetch('https://icanhazdadjoke.com/', config);
-  const data = await res.json();
-  jokeElement.innerHTML = data.joke;
+  try {
+    const config = {
+      headers: {
+        Accept: 'application/json',
+      },
+    };
+    const res = await fetch('https://icanhazdadjoke.com/', config);
+    const data = await res.json();
+    jokeElement.innerHTML = data.joke;
+  } catch (err) {
+    jokeElement.innerHTML = `<p><strong>${err.name}</strong></p><p>${err.message}</p>`;
+  }
 }
 
 // function generateJoke() {
